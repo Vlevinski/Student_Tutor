3910b41d5ba7d16ea8860ca7647a9fcee2310033
Valerian Levinski <v.levinski@gmail.com>
2020-01-20 10:58:39 +0200

    conf commit

--- Student_config.py
+++ Student_config.py
@@ -1,4 +1,5 @@
 #!python3.7
+
 ###
 #   App configurator
 ###
@@ -6,7 +7,7 @@ import configparser
 
 # string declaration
 cfg_data = '''
-[mysql]
+[sqlite]
 host = localhost
 user = user7
 passwd = s$cret
@@ -16,38 +17,8 @@ db = ydb
 #init config
 config = configparser.ConfigParser()
 config.read_string(cfg_data)
-'''
-# show parameters
-host, user, passwd, db = config['mysql'].values()
-print( host,user,passwd,db)
 
 # write to filr
-with open('conf/liteDB.ini', 'w') as configfile:
-    config.write(configfile)
+with open('conf/liteDB.ini', 'w') as config_file:
+    config.write(config_file)
 
-# read from file
-config1 = configparser.ConfigParser()
-config1.read('conf/liteDB.ini')
-
-# show file data
-host, user, passwd, db = config1['mysql'].values()
-print( host,user,passwd,db)
-
-#show file sections
-sections = config1.sections()
-print(f'Sections: {sections}')
-'''
-# create new section as dict
-cfg_data = {
-    'progresql': {'host': 'localhost', 'user': 'user5',
-              'passwd': 's$$ret', 'db': 'mdb'}
-}
-
-# new config section
-config2 = configparser.ConfigParser()
-config2.read_dict(cfg_data)
-
-# write to filr
-with open('conf/liteDB.ini', 'w') as configfile:
-    config.write(configfile)
-    config2.write(configfile)
