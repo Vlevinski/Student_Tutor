1d0d5cda9977c708002ed535aeeea744fc63324e
Valerian Levinski <v.levinski@gmail.com>
2020-01-22 13:03:22 +0200

    methods commit

--- methods/superClass.py
+++ methods/superClass.py
@@ -6,12 +6,14 @@ class Figure(object):
     def method(self):
         return self.one
 
+
 class Circle(Figure):
     def __init__(self, two):
-        Figure.__init__(self,two)
+        Figure.__init__(self, two)
         print("Circle init called.")
         self.two = two
 
+# class inheritance
 my_draw = Circle('Tor')
 print(my_draw.method())
 print(Circle.__mro__)
