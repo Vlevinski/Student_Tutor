238283b2f75fdf9bf06536c044d1633e5c621c17
Valerian Levinski <v.levinski@gmail.com>
2020-02-10 10:16:58 +0200

    last commit

--- y_if/__init__.py
+++ y_if/__init__.py
@@ -1,9 +1,14 @@
 #!python3.7
 #
-import random
+# import random
 
 
-def get_wordlist(x):
+def get_one(x):
+    """
+    # return the word related to value x in range 1-5
+    :param x:
+    :return:
+    """
     return ('foo' if (x == 1) else
             'bar' if (x == 2) else
             'baz' if (x == 3) else
@@ -12,11 +17,24 @@ def get_wordlist(x):
 
 
 def get_index(words, el):
+    """
+    # get index of the word in the words list
+    :param words:
+    :param el:
+    :return:
+    """
     return words.index(el)
 
 
-def get_word(words,idx):
+def get_word(words, idx):
+    """
+    # return the word with index idx
+    :param words:
+    :param idx:
+    :return:
+    """
     return words[idx]
+
 
 '''
 print([get_wordlist(random.randint(1, 4)) for item in range(10)])
