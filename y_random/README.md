e5427cd3059052ea3ad55e9ef28cafdacf8e9a0a
Valerian Levinski <v.levinski@gmail.com>
2020-02-12 10:29:17 +0200

    last commit

--- y_random/README.md
+++ y_random/README.md
@@ -1,7 +1,9 @@
 # ranfom functions as a lib functions
 
-r_init(a=None)- init internal state
-r_state()- get internal state object
+r_init(a=None)       - init internal state
+r_state()           - get internal state object
 r_int(a=None, b=None)- get random int in not mandatory range a,b
-r_range(a,b)-  get random int in mandatory arguments range a,b
+r_range(a,b)        - get random int in mandatory arguments range a,b
 
+r_random()          - get float in range 0.0 1.0
+r_number(a, b)      - return float number in range a,b:
--- y_random/__init__.py
+++ y_random/__init__.py
@@ -3,21 +3,34 @@
 
 import random
 
+
 def r_init(a=None):
     # init internal state
     return random.seed(a)
+
 
 def r_state():
     # get internal state object
     return random.getstate()
 
+
 def r_int(a=None, b=None):
     # get random int in not mandatory range a,b
-    if not a or not b: a=0; b=1
-    return random.randint(a,b)
+    if not a or not b:
+        a, b = 0, 1
+    return random.randint(a, b)
 
-def r_range(a,b):
+
+def r_range(a, b):
     # get random int in mandatory arguments range a,b
-    return random.randint(a,b)
+    return random.randint(a, b)
 
 
+def r_random():
+    # get float in range 0.0 1.0
+    return random.random()
+
+
+def r_number(a, b):
+    # return float number in range a,b:
+    return random.uniform(a, b)
