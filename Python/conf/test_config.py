d149923db671088a8049b813d9bdbf275744fb17
Valerian Levinski <v.levinski@gmail.com>
2020-02-13 09:44:27 +0200

    last commit

---
+++ Python/conf/test_config.py
@@ -0,0 +1,23 @@
+#!python3.7
+
+###
+#   App configurator
+###
+import configparser
+
+# string declaration
+cfg_data = '''
+[sqlite]
+host = localhost
+user = user7
+passwd = s$cret
+db = ydb
+'''
+
+#init config
+config = configparser.ConfigParser()
+config.read_string(cfg_data)
+
+# write to filr
+with open('conf/DB.ini', 'w') as config_file:
+    config.write(config_file)
