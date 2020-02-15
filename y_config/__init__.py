1848545e213213561f0d1981f31bcce27fef966c
Valerian Levinski <v.levinski@gmail.com>
2020-02-15 18:18:13 +0200

    last commit

--- y_config/__init__.py
+++ y_config/__init__.py
@@ -1,4 +1,10 @@
 def write_conf(config, path):
+    '''
+
+    :param config:  configparser instance
+    :param path:    config file path
+    :return:
+    '''
     with open(path, 'w') as f:
         config.write(f)
     return True
