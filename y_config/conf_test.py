bd0e486f327686992592511ab4e5729a404dd95f
Valerian Levinski <v.levinski@gmail.com>
2020-02-15 18:50:10 +0200

    last commit

--- y_config/conf_test.py
+++ y_config/conf_test.py
@@ -5,8 +5,8 @@ path = "conf.ini"
 config = configparser.ConfigParser()
 
 # create demo config
-cfg = set_string()
-config.read_string(cfg)
+#cfg = set_string()
+#config.read_string(cfg)
 
 # write_conf(config, path)
 
