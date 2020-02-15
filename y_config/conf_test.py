94094d92fa708006925ec2c6b2b2b88aee4596a0
Valerian Levinski <v.levinski@gmail.com>
2020-02-15 18:35:31 +0200

    last commit

--- y_config/conf_test.py
+++ y_config/conf_test.py
@@ -4,13 +4,7 @@ import configparser
 path = "conf.ini"
 config = configparser.ConfigParser()
 
-cfg = '''
-[data.ini]
-one = 1
-two = 27/01/05
-db = my_db
-col = False
-'''
-
+#create demo config
+cfg = set_string()
 config.read_string(cfg)
 write_conf(config, path)
